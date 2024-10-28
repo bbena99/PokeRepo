3a69f2a776cca5edd3c0e4219eee433047343267
Brock Bena <bbena99@gmail.com>
2024-10-28 17:14:30 -0500

    Added component routing with basic 2 pages back and forth

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -3,5 +3,8 @@ import './style.css'
 import App from './App.vue'
 
 import './index.css'
+import { router } from './router'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
---
+++ Client/src/pages/Pokemon.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1>Pokemon page works</h1>
+</template>
+
+<style scoped>
+</style>
