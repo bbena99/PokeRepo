a8b3dd41e9ee108fc4dd0e2acc0d92d6b75ff53b
Brock Bena <bbena99@gmail.com>
2024-10-30 14:05:27 -0500

    Made simple test cases to refresh my skills with Controller and routing requests.

--- Server/app/Http/Controllers/Controller.php
+++ Server/app/Http/Controllers/Controller.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Http\Request;
 abstract class Controller
 {
-    //
+    public function test(){
+        return view("test");
+    }
 }
--- Server/routes/web.php
+++ Server/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Routing\Controller;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/test', function () {
+    return view(view: 'test');
+});
+
+Route::get('/test2', [Controller::class, 'test'])->name('test');
