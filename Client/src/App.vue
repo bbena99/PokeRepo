9c505160120f11a5eb5089876092c489b47f9adb
Brock Bena <bbena99@gmail.com>
2024-11-02 17:00:52 -0500

    implemented theme changing

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -4,12 +4,20 @@ import  NavBar  from "./components/NavBar.vue";
 
 const theme = ref(localStorage.getItem('theme')??'light');
 const secondary = ref(localStorage.getItem('Theme')??'');
+const setTheme = (newTheme:string)=>{
+  localStorage.setItem('theme',newTheme);
+  theme.value=newTheme;
+}
+const setSecondary = (newTheme:string)=>{
+  localStorage.setItem('secondary',newTheme);
+  secondary.value=newTheme;
+}
 </script>
 
 <template>
-  <div :class="[theme,secondary]">
-    <NavBar />
-    <div class="w-screen overflow-scroll"style="height: calc(100vh - 5rem);">
+  <div :class="[theme,secondary]" class=" font-sans">
+    <NavBar :theme :setTheme :secondary :setSecondary/>
+    <div class="w-screen bg-gradient-to-b from-bg1 to-bg2 text-text"style="height: calc(100vh - 5rem);">
       <RouterView/>
     </div>
   </div>
--- Client/src/components/NavBar.vue
+++ Client/src/components/NavBar.vue
@@ -1,30 +1,43 @@
 <script setup lang="ts">
+const props = defineProps(['theme','setTheme','secondary','setSecondary'])
+
+function themeChanger(){
+  const newTheme = props.theme==='light'?('dark'):('light');
+  props.setTheme(newTheme);
+}
 </script>
 
 <template>
-  <nav class="bg-white border-gray-200 dark:bg-gray-900 h-20">
+  <nav class="bg-bg2 text-header">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <RouterLink to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
           <img src="../assets/images/Brock.png" class="h-12" alt="Flowbite Logo" />
-          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+          <span class="self-center text-2xl font-semibold whitespace-nowrap">
             Brock Wiki
           </span>
       </RouterLink>
-      <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+      <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden" aria-controls="navbar-default" aria-expanded="false">
           <span class="sr-only">Open main menu</span>
           <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
           </svg>
       </button>
       <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 [&>li]:flex">
           <li>
-            <RouterLink to="/Pokemon" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">
+            <RouterLink to="/Pokemon" class="block py-2 px-3 rounded md:bg-transparent" aria-current="page">
               Pokemon
             </RouterLink>
           </li>
           <li>
-            
+            <button type="button" @click="themeChanger()">
+              <svg v-if="props.theme==='light'" class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                <path fill-rule="evenodd" d="M13 3a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0V3ZM6.343 4.929A1 1 0 0 0 4.93 6.343l1.414 1.414a1 1 0 0 0 1.414-1.414L6.343 4.929Zm12.728 1.414a1 1 0 0 0-1.414-1.414l-1.414 1.414a1 1 0 0 0 1.414 1.414l1.414-1.414ZM12 7a5 5 0 1 0 0 10 5 5 0 0 0 0-10Zm-9 4a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2H3Zm16 0a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2h-2ZM7.757 17.657a1 1 0 1 0-1.414-1.414l-1.414 1.414a1 1 0 1 0 1.414 1.414l1.414-1.414Zm9.9-1.414a1 1 0 0 0-1.414 1.414l1.414 1.414a1 1 0 0 0 1.414-1.414l-1.414-1.414ZM13 19a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0v-2Z" clip-rule="evenodd"/>
+              </svg>
+              <svg v-else class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                <path fill-rule="evenodd" d="M11.675 2.015a.998.998 0 0 0-.403.011C6.09 2.4 2 6.722 2 12c0 5.523 4.477 10 10 10 4.356 0 8.058-2.784 9.43-6.667a1 1 0 0 0-1.02-1.33c-.08.006-.105.005-.127.005h-.001l-.028-.002A5.227 5.227 0 0 0 20 14a8 8 0 0 1-8-8c0-.952.121-1.752.404-2.558a.996.996 0 0 0 .096-.428V3a1 1 0 0 0-.825-.985Z" clip-rule="evenodd"/>
+              </svg>
+            </button>
           </li>
         </ul>
       </div>
