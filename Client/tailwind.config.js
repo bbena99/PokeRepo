4aed2e61a98f7c7452eff8fa36685e6cb7c7223b
Brock Bena <bbena99@gmail.com>
2024-11-02 17:00:29 -0500

    Added custom themes to the tailwind config

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -6,7 +6,16 @@ export default {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg1: "var(--color-bg1)",
+        bg2: "var(--color-bg2)",
+        header: "var(--color-header)",
+        text: "var(--color-text)",
+        hover: "var(--color-hover)",
+        btn: "var(--color-btn)",
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin'),
