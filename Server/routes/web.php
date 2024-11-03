35671834bbcdc2cf10a1823fc763533587b502ce
Brock Bena <bbena99@gmail.com>
2024-11-02 21:31:33 -0500

    removed some boilerplate that is unused currently

--- Server/routes/web.php
+++ Server/routes/web.php
@@ -3,9 +3,9 @@
 use App\Http\Controllers\PokeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 Route::get('/pokemon', [PokeController::class, 'getAll']);
 Route::get('/pokemon/{identifier}', [PokeController::class, 'getOne']);
 Route::get('/{parse}/{identifier}', [PokeController::class, 'getResource']);
