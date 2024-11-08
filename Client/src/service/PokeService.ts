555bece57eb03e844440356ede4895b868dbb777
Brock Bena <bbena99@gmail.com>
2024-11-08 14:03:01 -0600

    fixed the getResource() and added getParse() to service file.

--- Client/src/service/PokeService.ts
+++ Client/src/service/PokeService.ts
@@ -8,9 +8,25 @@ interface FiltersI{
   gen?:number;
   type?:number;
 }
-export function getResource(resource:standardPair){
+export function getResource(resource:standardPair,cb:(a:any)=>void){
+  console.log(typeof(resource))
   const splitUrl = resource.url.split('/');
-  console.log(splitUrl);
+  splitUrl.pop()
+  const identifier = splitUrl.pop();
+  const parse = splitUrl.pop();
+  getParse(parse!,identifier!,cb);
+}
+export function getParse(parse:string,identifier:string,cb:(a:any)=>void){
+  axios.get(`http://127.0.0.1:8000/${parse}/${identifier}`)
+  .then(res=>{
+    cb(JSON.parse(res.data));
+  })
+  .catch(err=>{
+    console.warn("err in PokeServe.ts/getResource()");
+    console.error(err);
+    cb(err);
+  });
+
 }
 export function getAll(filters:FiltersI,cb:(a:any)=>void):void{
   axios.get(`http://127.0.0.1:8000/pokemon?limit=${filters.limit}&offset=${filters.offset}`)
