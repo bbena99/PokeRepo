c25975f95900c2bf256709b0b39b3a49656e8598
Brock Bena <bbena99@gmail.com>
2024-11-09 20:38:27 -0600

    Added some early functionality

--- Client/src/components/DamageMultiplier.vue
+++ Client/src/components/DamageMultiplier.vue
@@ -1,24 +1,34 @@
 <script setup lang="ts">
-const props = defineProps(['mult']);
+import { standardPair } from '../models';
+
+const props = defineProps(['mult','type']);
 const obj = {mult:(+props.mult),color:''};
+const type:standardPair = props.type;
+console.log(type)
 switch(obj.mult){
   case 0:
     obj.color='bg-stone-900 text-gray-300';
-    break;
-  case 1:
-    obj.color='bg-gray-300 text-stone-800';
     break;
   case 2:
     obj.color='bg-yellow-400 text-gray-900';
     break;
   case 4:
     obj.color='bg-red-600 text-gray-900';
+    break;
+  default:
+    obj.color='bg-gray-300 text-stone-800';
+    break;
 }
 
 </script>
 
 <template>
-  <div :class="'w-full h-full rounded-full '+obj.color">
+  <div :class="'w-24 h-3/6 rounded-full pr-2 flex justify-between items-center '+obj.color">
+    <img 
+      :src="'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/generation-ix/scarlet-violet/'+type.url.split('/')[6]+'.png'"
+      :alt="type.name+'.png'"
+      class="w-20 h-full rounded-full"
+    >
     {{ obj.mult+'x' }}
   </div>
 </template>
