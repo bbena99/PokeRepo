f7e3cc0f697d46511a73197290584cb450feb4c6
Brock Bena <bbena99@gmail.com>
2024-11-15 13:37:50 -0600

    Added more tables and columns to the tables.

--- Server/database/migrations/2024_11_15_160906_pokemon.php
+++ Server/database/migrations/2024_11_15_160906_pokemon.php
@@ -14,6 +14,36 @@ return new class extends Migration
     //
     Schema::create('pokemon', function(Blueprint $table){
       $table->id();
+      $table->string('name');
+      $table->boolean('is_default');
+      $table->integer('order');
+      $table->string('front_sprite');
+      $table->string('back_sprite');
+      $table->json('stats');
+    });
+    Schema::create('abilities', function(Blueprint $table){
+      $table->id();
+      $table->string('name');
+      $table->string('effect_entries');
+    });
+    Schema::create('moves', function(Blueprint $table){
+      $table->id();
+      $table->string('name');
+      $table->tinyInteger('power');
+      $table->tinyInteger('pp');
+      $table->tinyInteger('priority');
+      $table->tinyInteger('effect_chance');
+      $table->string('effect_entry');
+      $table->json('meta');
+    });
+    Schema::create('items', function(Blueprint $table){
+      $table->id();
+      $table->string('name');
+    });
+    Schema::create('types', function(Blueprint $table){
+      $table->id();
+      $table->string('name');
+      $table->string('src');
     });
   }
 
