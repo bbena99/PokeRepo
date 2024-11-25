1373e961785466fcd79afd71d7a3d7897b024b52
Brock Bena <bbena99@gmail.com>
2024-11-25 11:05:04 -0600

    Finished debug print

--- Server/app/Models/PokeMove.php
+++ Server/app/Models/PokeMove.php
@@ -16,6 +16,7 @@ class PokeMove extends Model
   private $effect_chance;
   private $effect_entry;
   private $meta;
+
   public function setId($id_in):self{
     $this->id = $id_in;
     return $this;
@@ -81,9 +82,54 @@ class PokeMove extends Model
     return $this->effect_chance;
   }
   public function getEffectEntry():int{
-    return $this->effect_chance;
+    return $this->effect_entry;
   }
   public function getMeta(){
     return $this->meta;
+  }
+
+  public function debugPrint(){
+    $out = new \Symfony\Component\Console\Output\ConsoleOutput();
+    $out->writeln([
+      ".................................................................................",
+      "#".$this->id.":\t".$this->name,
+    ]);
+    switch ($this->damage_type) {
+      case 1:
+        $out->writeln("damage_type: (1)Status");
+        break;
+      case 2:
+        $out->writeln("damage_type: (2)Physical");
+        break;
+      case 3:
+        $out->writeln("damage_type: (3)Special");
+        break;
+      default:
+        $out->writeln("!!!invalid damage_type:".$this->damage_type);
+        break;
+    }
+    $out->writeln([
+      "power: ".$this->power."\t\tpp: ".$this->pp,
+      "accuracy: ".$this->accuracy."\tpriority: ".$this->priority,
+      "effect: ".$this->effect_chance."% to".$this->effect_entry,
+      "meta data:"
+    ]);
+    foreach($this->meta as $key => $value){
+      #$out->writeln(gettype($value));
+      switch (gettype($value)) {
+        case 'string':
+        case 'integer':
+        case 'NULL':
+          $out->writeln("-[".$key."] => ".$value);
+          break;
+        default:
+          $out->writeln("-[".$key."] =>");
+          foreach($value as $subKey => $subValue){
+            $out->writeln("---[".$subKey."] => ".$subValue);
+          }
+          break;
+      }
+    }
+    $out->writeln(".................................................................................");
   }
 }
