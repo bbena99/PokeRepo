6f3f54a7b3711b57ec0869d430afad4674091ec9
Brock Bena <bbena99@gmail.com>
2024-11-25 10:14:00 -0600

    Finished ability model, for now.

--- Server/app/Models/Ability.php
+++ Server/app/Models/Ability.php
@@ -6,6 +6,38 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ability extends Model
 {
-    private $id;
-    private $name;
+  private $id;
+  private $name;
+  private $effect_entry;
+
+  public function setID($id_in):self{
+    $this->id = $id_in;
+    return $this;
+  }
+  public function setName($name_in):self{
+    $this->name = $name_in;
+    return $this;
+  }
+  public function setEffectEntries($effect_entry_in):self{
+    $this->effect_entry = $effect_entry_in;
+    return $this;
+  }
+  public function getID():int{
+    return $this->id;
+  }
+  public function getName():string{
+    return $this->name;
+  }
+  public function getEffectEntry():string{
+    return $this->effect_entry;
+  }
+  public function debugPrint():void{
+    $out = new \Symfony\Component\Console\Output\ConsoleOutput();
+    $out->writeln([
+      ".................................................................................",
+      "#".$this->id.":\t".$this->name,
+      "Effect:\t".$this->effect_entry,
+      ".................................................................................",
+    ]);
+  }
 }
