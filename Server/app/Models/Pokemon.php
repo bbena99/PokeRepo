83cbecff4e28d650d33c0bdce3688f62eb867f59
Brock Bena <bbena99@gmail.com>
2024-12-08 14:17:31 -0600

    Start of documentation on Pokemon.php

--- Server/app/Models/Pokemon.php
+++ Server/app/Models/Pokemon.php
@@ -21,54 +21,109 @@ class Pokemon extends Model
   private array     $abilities;
   private array     $moves;
 
+  /**
+   * @param int $id_in is the ID of $this Pokemon.
+   */
   public function setId(int $id_in):self{
     $this->id = $id_in;
     return $this;
   }
+  /**
+   * @param string $name_in is the Name of $this Pokemon.
+   */
   public function setName(string $name_in):self{
     $this->name = $name_in;
     return $this;
   }
+  /**
+   * @param bool $is_default_in sets the $is_default of $this Pokemon.
+   */
   public function setIsDefault(bool $is_default_in):self{
     $this->is_default = $is_default_in;
     return $this;
   }
+  /**
+   * @param int $order_in sets the $order of $this Pokemon.
+   */
   public function setOrder(int $order_in):self{
     $this->order = $order_in;
     return $this;
   }
+  /**
+   * @param string $front_sprite_in sets the $front_sprite of $this Pokemon.
+   */
   public function setFrontSprite(string $front_sprite_in):self{
     $this->front_sprite = $front_sprite_in;
     return $this;
   }
+  /**
+   * @param string $back_sprite_in sets the $back_sprite of $this Pokemon.
+   */
   public function setBackSprite(string $back_sprite_in):self{
     $this->back_sprite = $back_sprite_in;
     return $this;
   }
+  /**
+   * @param array<string,int> $stats_array_in ($stat_name => $stat_value)
+   */
   public function setStats(array $stats_array_in):self{
     $this->stats = $stats_array_in;
     return $this;
   }
-  public function setSingleStat(int $value, string $index):self{
+  /**
+   * @param string  $index is the name of the stat.
+   * @param int     $value is the value of the stat.
+   */
+  public function setSingleStat(string $index, int $value):self{
     $this->stats[$index] = $value;
     return $this;
   }
+  /**
+   * @param array<int,object> $types_in (index => {int id, string name})
+   */
   public function setTypes(array $types_in) : self {
     $this->types = $types_in;
     return $this;
   }
-  public function setSingleType(array $type_in, int $index):self{
+  /**
+   * @param int $index index of type.
+   * @param array $type_in [int 'id', string 'name'].
+   */
+  public function setSingleType( int $index, array $type_in):self{
     $this->types[$index]=$type_in;
     return $this;
   }
+  /**
+   * @param array<int,array> $abilities_in (index => [int 'ability_id', string 'name', bool 'is_hidden'])
+   */
   public function setAbilities(array $abilities_in):self{
     $this->abilities = $abilities_in;
     return $this;
   }
-  public function setSingleAbility(array $ability, int $index):self{
+  /**
+   * @param int $index index of ability
+   * @param array $ability [int 'ability_id', string 'name', bool 'is_hidden']
+   */
+  public function setSingleAbility(int $index, array $ability):self{
     $this->abilities[$index]=$ability;
     return $this;
   }
+  /**
+   * @param array<object> $moves_in
+   * (index => {
+   *  object 'move' {
+   *    string name,
+   *    string url
+   *  },
+   *  array<int,object> version_group_details [version_group_index => {
+   *    int level_learned_at,
+   *    object move_learn_method {
+   *      string name,
+   *      string url
+   *    }
+   *  }]
+   * })
+   */
   public function setMoves(array $moves_in):self{
     foreach($moves_in as $move_in){
       $id = $this->parseIdentifier($move_in->move->url);
