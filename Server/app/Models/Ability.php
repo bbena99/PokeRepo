a4afef37704dd0371c826f554537e9ba1578282b
Brock Bena <bbena99@gmail.com>
2024-12-16 10:19:11 -0600

    Updated documentation of the function in the Ability class

--- Server/app/Models/Ability.php
+++ Server/app/Models/Ability.php
@@ -9,25 +9,42 @@ class Ability extends Model
   private $id;
   private $name;
   private $effect_entry;
-
+  /**
+   * @param int $id_in is the ID of $this Ability.
+   */
   public function setID($id_in):self{
     $this->id = $id_in;
     return $this;
   }
+  /**
+   * @param string $name_in is the Name of $this Ability.
+   */
   public function setName($name_in):self{
     $this->name = $name_in;
     return $this;
   }
-  public function setEffectEntries($effect_entry_in):self{
+  /**
+   * @param string $effect_entry_in is the effect of $this Ability.
+   */
+  public function setEffectEntry($effect_entry_in):self{
     $this->effect_entry = $effect_entry_in;
     return $this;
   }
+  /**
+   * @return int $id of $this Ability
+   */
   public function getID():int{
     return $this->id;
   }
+  /**
+   * @return string $name of $this Ability
+   */
   public function getName():string{
     return $this->name;
   }
+  /**
+   * @return string $effect_entry of $this Ability
+   */
   public function getEffectEntry():string{
     return $this->effect_entry;
   }
