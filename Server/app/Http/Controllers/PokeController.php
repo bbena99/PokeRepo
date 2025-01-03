6e605e49316d4ee7374d9c38754f65fbf68989a1
Brock Bena <bbena99@gmail.com>
2025-01-03 14:44:06 -0600

    Updates for DB

--- Server/app/Http/Controllers/PokeController.php
+++ Server/app/Http/Controllers/PokeController.php
@@ -25,32 +25,38 @@ class PokeController{
   public function getAll(Request $request){
     $this->limit = $request->query('limit','20');
     $this->offset = $request->query('offset','0');
-    $dbPokemon = DB::table('pokemon')->where('id','=',4000)->count();
-    if($dbPokemon){
-      $respon = true;
-    }else {
-      $respon = false;
-    }
-    return response()->json($respon);
-
+    $dbPokemon = DB::table('pokemon')->get();
     foreach($dbPokemon as $pokemon){
       $types = DB::table('relation_pokemon_type')->where('pokemon_id','=',$pokemon->id)->get();
       foreach($types as $type){
-        $pokemon->types[$type->type_id] = DB::table('types')->where('id','=',$type->type_id)->get();
+        $dbtype = DB::table('types')->where('id','=',$type->type_id)->get();
+        unset($dbtype["id"]);
+        $pokemon->types[(int)$type->type_id]=$dbtype;
       }
       $dbstats = DB::table('relation_pokemon_stat')->where('pokemon_id','=',$pokemon->id)->get();
-
+      foreach($dbstats as $dbstat){
+        $pokemon->stats[$dbstat->stat_name]=$dbstat->base_stat;
+      }
     }
-    return response()->json($dbPokemon);
-    return response()->json($pokeArray,200,[],JSON_PRETTY_PRINT)
+    return response()->json($dbPokemon)
       ->header('Access-Control-Allow-Origin', '*')
       ->header('Access-Control-Allow-Methods', 'GET');
   }
   public function getOne($identifier){
-    $poke = json_decode($this->api->pokemon($identifier),true);
-    return response($poke,200,[]);
-      // ->header('Access-Control-Allow-Origin', '*')
-      // ->header('Access-Control-Allow-Methods', 'GET');
+    $poke = DB::table('pokemon')->where('id','=',$identifier)->get()[0];
+    $types = DB::table('relation_pokemon_type')->where('pokemon_id','=',$poke->id)->get();
+    foreach($types as $type){
+      $dbtype = DB::table('types')->where('id','=',$type->type_id)->get();
+      unset($dbtype["id"]);
+      $poke->types[(int)$type->type_id]=$dbtype;
+    }
+    $dbstats = DB::table('relation_pokemon_stat')->where('pokemon_id','=',$poke->id)->get();
+    foreach($dbstats as $dbstat){
+      $poke->stats[$dbstat->stat_name]=$dbstat->base_stat;
+    }
+    return response()->json($poke)
+      ->header('Access-Control-Allow-Origin', '*')
+      ->header('Access-Control-Allow-Methods', 'GET');
   }
 }
 
