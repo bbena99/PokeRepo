95f329f3a9d8dec5c9a02202c215c6cf8be208de
Brock Bena <bbena99@gmail.com>
2025-01-06 12:08:27 -0600

    removed a function call.

--- Server/routes/web.php
+++ Server/routes/web.php
@@ -10,5 +10,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/initdb/{key}', [dbgen::class, 'initDb']);
 Route::get('/pokemon', [PokeController::class, 'getAll']);
 Route::get('/pokemon/{identifier}', [PokeController::class, 'getOne']);
-Route::get('/{parse}/{identifier}', [PokeController::class, 'getResource']);
 
