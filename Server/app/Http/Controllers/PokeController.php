ff7305c19798db7bfeb7a22c13775ad6f556b856
Brock Bena <bbena99@gmail.com>
2025-01-06 12:13:19 -0600

    removed reference code

--- Server/app/Http/Controllers/PokeController.php
+++ Server/app/Http/Controllers/PokeController.php
@@ -73,23 +73,3 @@ class PokeController{
       ->header('Access-Control-Allow-Methods', 'GET');
   }
 }
-
-// <?php
-
-// namespace App\Http\PokeControllers;
-
-// use Illuminate\Routing\Controller;
-// use PokePHP\PokeApi;
-
-// abstract class PokeController extends Controller{
-//   protected $api;
-
-//   public function __construct(){
-//     $this->api = new PokeApi();
-//   }
-//   public function test(){
-//     return response()->json($this->api->pokemon('10'))
-//       ->header('Access-Control-Allow-Origin', '*')
-//       ->header('Access-Control-Allow-Methods', 'GET');
-//   }
-// }
