241ff6548bc04024de929f1a8a11eac934f04f96
Brock Bena <bbena99@gmail.com>
2025-01-13 14:16:22 -0600

    Added loading and pokemon not found states. Cleaned up the abilities table.

--- Client/src/pages/Pokemon/[identifier].vue
+++ Client/src/pages/Pokemon/[identifier].vue
@@ -2,14 +2,17 @@
 //@ts-ignore ts says there is not export at this location, but there is.
 import { useRoute } from 'vue-router/auto';
 import { ref } from 'vue';
-import { getOne, getParse } from '../../service';
+import { getOne } from '../../service';
 import { emptyPokemon, PokémonI, TypeI } from '../../models';
+import Loading from '../../components/Loading.vue';
+import PageNotFound from '../../components/PageNotFound.vue'
 import { TYPES } from '../../constants';
 import DamageMultiplier from '../../components/DamageMultiplier.vue';
 import { RouterLink } from 'vue-router';
 
 const route = useRoute('/Pokemon/[identifier]');
 
+const state = ref<number>(0);
 const curPokemon = ref<PokémonI>(emptyPokemon());
 const baseStatTotal = ref<number>(0);
 const typeData = ref<TypeI[]>([]);
@@ -23,26 +26,33 @@ const STAT_COLOR = [
 ]
 
 getOne(route.params.identifier, (cb: PokémonI) => {
-  curPokemon.value = cb
-  baseStatTotal.value = Object.values(curPokemon.value.stats).reduce((acc, value) => acc + value, 0);
+  console.log(cb);
+  if(typeof(cb)==='string'){
+    state.value = -1;
+  }else{
+    curPokemon.value = cb
+    baseStatTotal.value = Object.values(curPokemon.value.stats).reduce((acc, value) => acc + value, 0);
+    state.value = 1;
+  }
 });
 </script>
 
 <template>
-  <div class="flex justify-center items-center">
+  <Loading v-if="state===0"/>
+  <div v-if="state===1" class="flex justify-center items-center overflow-hidden">
     <div class="grid grid-cols-5 gap-4 w-full h-full xl:w-2/3 overflow-y-scroll">
       <!--Pokemon name, types, and sprites-->
-      <div class="w-full h-72 p-4 col-span-5 rounded-lg border-2 border-bg2 mt-3 flex flex-wrap bg-bg1 shadow-xl">
-        <div class="w-2/3 flex flex-wrap">
-          <h1 class="w-full text-header text-7xl">
-            {{ curPokemon.name.charAt(0).toUpperCase() + curPokemon.name.slice(1) }}
-          </h1>
-          <img v-for="t in curPokemon.types" :src="t.src" :alt="t.name + '.png'" class="max-h-12 md:mx-4">
-        </div>
-        <div class="flex justify-center w-1/3 bg-bg2 rounded-full">
+      <div class="w-full h-72 p-4 col-span-5 rounded-lg border-2 border-bg2 mt-3 grid grid-cols-3 bg-bg1 shadow-xl">
+        <h1 class="col-span-2 w-full text-header text-7xl">
+          {{ curPokemon.name.charAt(0).toUpperCase() + curPokemon.name.slice(1) }}
+        </h1>
+        <div class="flex row-span-2 justify-center w-full bg-bg2 rounded-full">
           <img class="w-1/2 aspect-square" :src="curPokemon.front_sprite" alt="front-sprite.png">
           <img class="w-1/2 aspect-square" :src="curPokemon.back_sprite" v-if="curPokemon.back_sprite"
-            alt="back-default.png">
+          alt="back-default.png">
+        </div>
+        <div>
+          <img v-for="t in curPokemon.types" :src="t.src" :alt="t.name + '.png'" class="max-h-12 md:mx-4">
         </div>
       </div>
       <!--Pokemon Abilities-->
@@ -52,11 +62,11 @@ getOne(route.params.identifier, (cb: PokémonI) => {
             <tr class="w-full">
               <th colspan="3">
                 <span class="text-header text-lg font-semibold w-full">
-                  {{ curPokemon.name.charAt(0).toUpperCase() + curPokemon.name.slice(1) }}'s abilities:
+                  {{ curPokemon.name.charAt(0).toUpperCase() + curPokemon.name.slice(1) }}'s Abilities:
                 </span>
               </th>
             </tr>
-            <tr>
+            <tr class="*:border-2 *:border-text *:text-header *:bg-bg2">
               <th>
                 Name
               </th>
@@ -70,11 +80,12 @@ getOne(route.params.identifier, (cb: PokémonI) => {
           </thead>
           <tbody>
             <tr
+              class="*:border-2 *:border-text *:p-1"
               v-for="ability in curPokemon.abilities"
             >
               <td>
-                <RouterLink :to="'ability/'+ability.name">
-                  {{ ability.name }}
+                <RouterLink :to="'../Abilities/'+ability.name" class="hover:text-hover underline">
+                  {{ ability.name.charAt(0).toUpperCase() + ability.name.slice(1) }}
                 </RouterLink>
               </td>
               <td>
@@ -99,7 +110,7 @@ getOne(route.params.identifier, (cb: PokémonI) => {
       <!--Pokemon Stats-->
       <div class="w-full p-4 col-span-4 rounded-lg border-2 border-bg2 mt-3 flex flex-wrap bg-bg1 shadow-xl">
         <span class="text-header text-lg font-semibold w-full">
-          {{ curPokemon.name.charAt(0).toUpperCase() + curPokemon.name.slice(1) }}'s stats:
+          {{ curPokemon.name.charAt(0).toUpperCase() + curPokemon.name.slice(1) }}'s Stats:
         </span>
         <div v-for="(stat, key, index) in curPokemon.stats" :id="curPokemon.name + '_' + key" :key="curPokemon.name + key"
           class="flex justify-between max-w-full h-6 m-1 pr-2 text-bg1 bg-text rounded-full overflow-hidden"
@@ -123,6 +134,7 @@ getOne(route.params.identifier, (cb: PokémonI) => {
       </div>
     </div>
   </div>
+  <PageNotFound v-if="state===-1" header="Pokemon" message="Sent from /Pokemon/[Identifier]"/>
 </template>
 
 <style scoped></style>
