348dee8935cfd6c4789fb835f07fc3458b029907
Brock Bena <bbena99@gmail.com>
2025-01-13 14:16:32 -0600

    Added loading and pokemon not found states.

---
+++ Client/src/components/PageNotFound.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const props = defineProps(['header','message'])
+</script>
+
+<template>
+  <span>
+    {{props.header}} Not found works!
+    Message sent: {{ props.message }}
+  </span>
+</template>
+
+<style scoped>
+</style>
