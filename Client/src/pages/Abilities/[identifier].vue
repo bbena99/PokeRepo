e8b3bd9520c8dce737ec0e8abd1362c8d57e9df1
Brock Bena <bbena99@gmail.com>
2025-02-03 18:09:51 -0600

    Start of adding code to the components

--- Client/src/pages/Abilities/[identifier].vue
+++ Client/src/pages/Abilities/[identifier].vue
@@ -1,13 +1,32 @@
 <script lang="ts">
-import { useRoute } from 'vue-router';
+//@ts-ignore ts says there is not export at this location, but there is.
+import { useRoute } from 'vue-router/auto';
+import { ref } from 'vue';
+import Loading from "../../components/Loading.vue";
+import PageNotFound from '../../components/PageNotFound.vue';
+import { getOneAbility } from '../../service';
+import { AbilityI } from '../../models';
 
-const route = useRoute('/Pokemon/[identifier]');
+const route = useRoute('/Abilities/[identifier]');
+const state = ref<number>(0);
+const ability = ref<AbilityI>();
+getOneAbility(route.params.identifier,(a)=>{
+  if(typeof(a)==='string'){
+    state.value=-1
+  } else {
+    ability.value=a;
+    state.value=1;
+  }
+})
+
 </script>
 
 <template>
-  <span>
-    Abilities identifier Page works
-  </span>
+  <Loading v-if="state===0"/>
+  <div v-if="state===1">
+    {{ state.identifier }} works!
+  </div>
+  <PageNotFound v-if="state===-1" header="Ability" :message="'Ability: '+route.identifier+' was not found.'"/>
 </template>
 
 <style scoped>
--- Client/src/pages/moves/[identifier].vue
+++ Client/src/pages/moves/[identifier].vue
@@ -1,14 +1,20 @@
 <script lang="ts">
+import { ref } from 'vue';
 import { useRoute } from 'vue-router';
+import Loading from "../../components/Loading.vue";
+import PageNotFound from '../../components/PageNotFound.vue';
 
-const route = useRoute('/Pokemon/[identifier]');
+const route = useRoute('/Moves/[identifier]');
+const state = ref<number>(0);
 
 </script>
 
 <template>
-  <span>
-    Moves identifier Page works
-  </span>
+  <Loading v-if="state===0"/>
+  <div v-if="state===1">
+    {{ state.identifier }} works!
+  </div>
+  <PageNotFound v-if="state===-1"/>
 </template>
 
 <style scoped>
