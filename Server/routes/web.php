80a649ef2e464a0c24c33445d8009d458f0caa0d
Brock Bena <bbena99@gmail.com>
2025-02-05 09:56:23 -0600

    Added routes for new controllers

--- Server/routes/web.php
+++ Server/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\AbilityController;
 use App\Http\Controllers\dbgen;
+use App\Http\Controllers\MoveController;
 use App\Http\Controllers\PokeController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,4 +12,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/initdb/{key}', [dbgen::class, 'initDb']);
 Route::get('/pokemon', [PokeController::class, 'getAll']);
 Route::get('/pokemon/{identifier}', [PokeController::class, 'getOne']);
-
+Route::get('/ability', [AbilityController::class, 'getAll']);
+Route::get('/ability/{identifier}',[AbilityController::class, 'getOne']);
+Route::get('/move', [MoveController::class, 'getAll']);
+Route::get('/move/{identifier}',[MoveController::class, 'getOne']);
