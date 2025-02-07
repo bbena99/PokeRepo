27f39eb78c2cc823a47ede71c9d06aac52d21efa
Brock Bena <bbena99@gmail.com>
2025-02-07 12:19:23 -0600

    Added functionality to the getOne() method.

--- Server/app/Http/Controllers/AbilityController.php
+++ Server/app/Http/Controllers/AbilityController.php
@@ -28,6 +28,28 @@ class AbilityController{
     if(filter_var($identifier, FILTER_VALIDATE_INT))$dbAbility = DB::table('abilities')->where('id','=',$identifier)->get()[0];
     else $dbAbility = DB::table('abilities')->where('name','=',$identifier)->get()[0];
 
+    $relation = DB::table('relation_pokemon_abilities')
+        ->where('ability_id','=',$dbAbility->id)
+        ->get();
+
+    $dbAbility->pokemon = [];
+    $dbAbility->hiddenPokemon = [];
+    foreach($relation as $poke){
+      $dbPoke = DB::table('pokemon')
+          ->where('id','=',$poke->pokemon_id)
+          ->get()[0];
+      $dbPoke->types = [];
+      foreach(DB::table('relation_pokemon_type')
+          ->where('pokemon_id','=',$poke->pokemon_id)
+          ->select('type_id')
+          ->get() as $dbResult){
+        array_push($dbPoke->types,$dbResult->type_id);
+      }
+      if($poke->hidden)array_push($dbAbility->hiddenPokemon,$dbPoke);
+      else array_push($dbAbility->pokemon,$dbPoke);
+
+    }
+
     return response()->json($dbAbility)
       ->header('Access-Control-Allow-Origin', '*')
       ->header('Access-Control-Allow-Methods', 'GET');
