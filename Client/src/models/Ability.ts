bd733103dd8b6d1cfaafd524b77c3400856d4a9b
Brock Bena <bbena99@gmail.com>
2025-02-07 13:48:20 -0600

    Updated model to match the backend data.

--- Client/src/models/Ability.ts
+++ Client/src/models/Ability.ts
@@ -1,4 +1,19 @@
+import { PokémonI } from "./Pokémon";
+
 export interface AbilityI{
   id:number;
-  name:string
+  name:string;
+  effect_entries:string;
+  pokemon:PokémonI[];
+  hiddenPokemon:PokémonI[];
+}
+
+export function emptyAbility():AbilityI{
+  return {
+    id:-1,
+    name:"",
+    effect_entries:"",
+    pokemon:[],
+    hiddenPokemon:[],
+  }
 }
