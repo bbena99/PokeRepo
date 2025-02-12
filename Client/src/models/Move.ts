14ee470bea71535682851fd27a3da870fa4fd4af
Brock Bena <bbena99@gmail.com>
2025-02-12 09:38:18 -0600

    Added extra baseline data from backend

--- Client/src/models/Move.ts
+++ Client/src/models/Move.ts
@@ -1,11 +1,29 @@
 export interface MovesI{
   id:number;
   name:string;
+  machine:string|undefined;
+  damage_type:number;
+  effect_entry:string;
+  effect_chance:number;
+  pp:number;
+  power:number;
+  priority:number;
+  accuracy:number;
+  meta:any;
 }
 
 export function emptyMove():MovesI{
   return {
     id:-1,
-    name:""
+    name:"",
+    machine:undefined,
+    damage_type:-1,
+    effect_entry:"",
+    effect_chance:-1,
+    pp:-1,
+    power:-1,
+    priority:-1,
+    accuracy:-1,
+    meta:{},
   }
 }
