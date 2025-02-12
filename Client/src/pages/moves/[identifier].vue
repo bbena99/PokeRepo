26fa506f014b7dc59ca2f8ec1f8d14a3b68dbad7
Brock Bena <bbena99@gmail.com>
2025-02-12 09:38:41 -0600

    added fetch for backend data

--- Client/src/pages/moves/[identifier].vue
+++ Client/src/pages/moves/[identifier].vue
@@ -3,10 +3,21 @@ import { ref } from 'vue';
 import { useRoute } from 'vue-router';
 import Loading from "../../components/Loading.vue";
 import PageNotFound from '../../components/PageNotFound.vue';
+import { emptyMove, MovesI } from '../../models/Move';
+import { getOneMove } from '../../service';
 
 const route = useRoute('/Moves/[identifier]');
 const state = ref<number>(0);
+const move = ref<MovesI>(emptyMove());
 
+getOneMove(route.params.identifier,(m)=>{
+  if(typeof(m)==='string'){
+    state.value=-1;
+  } else {
+    move.value=m;
+    state.value=1;
+  }
+})
 </script>
 
 <template>
@@ -14,7 +25,7 @@ const state = ref<number>(0);
   <div v-if="state===1">
     {{ route.params.identifier }} works!
   </div>
-  <PageNotFound v-if="state===-1"/>
+  <PageNotFound v-if="state===-1" />
 </template>
 
 <style scoped>
