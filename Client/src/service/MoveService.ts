d160f16e6b38ed3aeaad318882711e4c80629c28
Brock Bena <bbena99@gmail.com>
2025-02-12 09:38:00 -0600

    Converted ability text to move text

--- Client/src/service/MoveService.ts
+++ Client/src/service/MoveService.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { AbilityI } from "../models";
+import { MovesI } from "../models/Move";
 
 interface FiltersI{
   limit:number;
@@ -8,26 +8,27 @@ interface FiltersI{
 
 const url='http://127.0.0.1:8000/';
 
-export function getAllMoves(filters:FiltersI,cb:(a:AbilityI[])=>void):void{
+export function getAllMoves(filters:FiltersI,cb:(a:MovesI[])=>void):void{
   axios.get(`${url}move?limit=${filters.limit}&offset=${filters.offset}`)
     .then(res=>{
-      const serverPokeJSON:AbilityI[] = res.data;
+      const serverPokeJSON = res.data;
       cb(serverPokeJSON);
     })
     .catch(err=>{
-      console.warn("err in AbilityService.ts/getAll()");
+      console.warn("err in MoveService.ts/getAllMoves()");
       console.error(err);
       cb(err);
     });
 }
-export function getOneMove(id:string,cb:(a:AbilityI)=>void):void{
-  axios.get(`${url}ability/`+id)
+export function getOneMove(id:string,cb:(a:MovesI)=>void):void{
+  axios.get(`${url}move/`+id)
     .then(res=>{
-      const ability:AbilityI = res.data;
-      cb(ability);
+      const move = res.data;
+      console.log(move)
+      cb(move);
     })
     .catch(err=>{
-      console.warn("err in AbilityService.ts/getOneAbility()");
+      console.warn("err in MoveService.ts/getOneMove()");
       console.error(err);
       cb(err.message);
     })
