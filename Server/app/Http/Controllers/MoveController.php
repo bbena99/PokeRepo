7ce3aa23724f833334c7ca0193907e3fa617b755
Brock Bena <bbena99@gmail.com>
2025-02-12 09:31:47 -0600

    Added some basic code to be built upon later once the data is more flushed out.

--- Server/app/Http/Controllers/MoveController.php
+++ Server/app/Http/Controllers/MoveController.php
@@ -4,20 +4,31 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MoveController{
-    protected $output;
-    protected $limit;
-    protected $offset;
+  protected $output;
+  protected $limit;
+  protected $offset;
 
-    public function __construct(){
-      $this->output = new \Symfony\Component\Console\Output\ConsoleOutput();
-    }
+  public function __construct(){
+    $this->output = new \Symfony\Component\Console\Output\ConsoleOutput();
+  }
 
-    public function getAll(Request $request){
+  public function getAll(Request $request){
+    $this->limit = $request->query('limit','20');
+    $this->offset = $request->query('offset','0');
+    $dbMove = DB::table('move')->get();
+    return response()->json($dbMove)
+        ->header('Access-Control-Allow-Origin', '*')
+        ->header('Access-Control-Allow-Methods', 'GET');
+  }
+  public function getOne($identifier){
+    if(filter_var($identifier, FILTER_VALIDATE_INT))$dbMove = DB::table('moves')->where('id','=',$identifier)->get()[0];
+    else $dbMove = DB::table('moves')->where('name','=',$identifier)->get()[0];
 
-    }
-    public function getOne(Request $request){
-
-    }
+    return response()->json($dbMove)
+      ->header('Access-Control-Allow-Origin', '*')
+      ->header('Access-Control-Allow-Methods', 'GET');
+  }
 }
