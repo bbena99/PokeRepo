0e4073ba3d63bb03dea826f50546e3a47863f4e2
Brock Bena <bbena99@gmail.com>
2025-02-19 17:20:41 -0600

    Added code for pokemon array related to pokemon that learn that move

--- Server/app/Http/Controllers/MoveController.php
+++ Server/app/Http/Controllers/MoveController.php
@@ -26,7 +26,18 @@ class MoveController{
   public function getOne($identifier){
     if(filter_var($identifier, FILTER_VALIDATE_INT))$dbMove = DB::table('moves')->where('id','=',$identifier)->get()[0];
     else $dbMove = DB::table('moves')->where('name','=',$identifier)->get()[0];
+    $dbMove->pokemon = [];
+    foreach( DB::table('relation_pokemon_moves')
+          ->where('move_id','=',$dbMove->id)
+          ->join('pokemon','relation_pokemon_moves.pokemon_id','=','pokemon.id')
+          ->get()
+        as $dbPokemon
+    ){
+      $types = [];
+      foreach( DB::table('relation_pokemon_type')->where('pokemon_id','=',$dbPokemon->id)->get() as $dbType ){
 
+      }
+    }
     return response()->json($dbMove)
       ->header('Access-Control-Allow-Origin', '*')
       ->header('Access-Control-Allow-Methods', 'GET');
