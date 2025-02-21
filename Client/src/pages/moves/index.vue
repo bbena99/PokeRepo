d183329e322bf5abd96497f9214cf962d3dfc767
Brock Bena <bbena99@gmail.com>
2025-02-21 13:41:44 -0600

    Changed typo from singular to plural to match the router

--- Client/src/pages/moves/index.vue
+++ Client/src/pages/moves/index.vue
@@ -22,7 +22,7 @@ getAllMoves({
   <Loading v-if="state===0"/>
   <div v-if="state===1" class="flex justify-center items-center py-4">
     <div class="grid grid-cols-12 gap-3 xl:w-2/3">
-      <RouterLink v-for="move in moveArray" :to="'move/'+move.name" class="col-span-6 md:col-span-4 xl:col-span-3 2xl:col-span-2 rounded-xl shadow-lg border-2 border-bg2 bg-bg1 p-2 text-lg underline hover:text-hover hover:border-hover hover:shadow-2xl hover:scale-105">
+      <RouterLink v-for="move in moveArray" :to="'Moves/'+move.name" class="col-span-6 md:col-span-4 xl:col-span-3 2xl:col-span-2 rounded-xl shadow-lg border-2 border-bg2 bg-bg1 p-2 text-lg underline hover:text-hover hover:border-hover hover:shadow-2xl hover:scale-105">
         {{ move.name.charAt(0).toUpperCase() + move.name.slice(1).replace('-',' ') }}
       </RouterLink>
     </div>
