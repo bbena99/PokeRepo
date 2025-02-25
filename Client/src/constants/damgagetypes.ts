e3ae4233b0332e454cb8a63d71c4df0f692ca308
Brock Bena <bbena99@gmail.com>
2025-02-24 20:19:51 -0600

    Added damage types

---
+++ Client/src/constants/damgagetypes.ts
@@ -0,0 +1,16 @@
+import status from "../assets/images/status.png";
+import physical from "../assets/images/physical.png";
+import special from "../assets/images/special.png";
+
+export const DAMAGETYPES=[
+  {
+    name:"status",
+    src: status
+  },{
+    name:"physical",
+    src: physical
+  },{
+    name:"special",
+    src:special
+  }
+];
