f46ff48ea9f036e73f5a4bf044bd0325332b51b8
Brock Bena <bbena99@gmail.com>
2025-02-28 09:16:34 -0600

    Added types and images to the moves

--- Client/src/pages/moves/index.vue
+++ Client/src/pages/moves/index.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue';
 import { getAllMoves } from '../../service';
 import Loading from '../../components/Loading.vue';
 import PageNotFound from '../../components/PageNotFound.vue';
+import { DAMAGETYPES } from '../../constants';
 import { MovesI } from '../../models';
 import { RouterLink } from 'vue-router';
 
@@ -22,8 +23,20 @@ getAllMoves({
   <Loading v-if="state===0"/>
   <div v-if="state===1" class="flex justify-center items-center py-4">
     <div class="grid grid-cols-12 gap-3 xl:w-2/3">
-      <RouterLink v-for="move in moveArray" :to="'Moves/'+move.name" class="col-span-6 md:col-span-4 xl:col-span-3 2xl:col-span-2 rounded-xl shadow-lg border-2 border-bg2 bg-bg1 p-2 text-lg underline hover:text-hover hover:border-hover hover:shadow-2xl hover:scale-105">
+      <RouterLink v-for="move in moveArray" :to="'Moves/'+move.name" class="flex justify-between col-span-6 md:col-span-4 xl:col-span-3 rounded-xl shadow border-2 border-bg2 bg-bg1 p-2 text-lg underline hover:text-hover hover:border-hover hover:shadow-2xl hover:scale-105">
         {{ move.name.charAt(0).toUpperCase() + move.name.slice(1).replace('-',' ') }}
+        <div class="flex w-28 h-8">
+          <img 
+            :src="DAMAGETYPES[move.damage_type].src"
+            :alt="DAMAGETYPES[move.damage_type].src"
+            class="w-1/3 h-full"
+          >
+          <img
+            :src="'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/generation-ix/scarlet-violet/'+move.type_id+'.png'"
+            :alt="'type'+move.type_id"
+            class="w-2/3 h-full"
+          >
+        </div>
       </RouterLink>
     </div>
   </div>
