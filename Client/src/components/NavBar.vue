9032b2425d73405d474e5a3cd1adc0b2a99bd6f3
Brock Bena <bbena99@gmail.com>
2025-03-03 15:43:31 -0600

    Fixed issue where the menuBars button wasn't opening the menu

--- Client/src/components/NavBar.vue
+++ Client/src/components/NavBar.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
-const props = defineProps(['theme','setTheme','secondary','setSecondary'])
+import { faBars, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { ref } from 'vue';
 
+const props = defineProps(['theme','setTheme','secondary','setSecondary'])
+const display = ref<string>("hidden ");
+function displayChanger(){
+  display.value = display.value==="hidden "?"inline-block ":"hidden ";
+}
 function themeChanger(){
   const newTheme = props.theme==='light'?('dark'):('light');
   props.setTheme(newTheme);
@@ -16,14 +23,12 @@ function themeChanger(){
             Brock Wiki
           </span>
       </RouterLink>
-      <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden" aria-controls="navbar-default" aria-expanded="false">
+      <button type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden" @click="displayChanger()">
           <span class="sr-only">Open main menu</span>
-          <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-          </svg>
+          <FontAwesomeIcon class="text-2xl" :icon="faBars"/>
       </button>
-      <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 [&>li]:flex">
+      <div :class="display+'w-full h-auto md:block md:w-auto'" id="navbar-default">
+        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 [&>li]:flex">
           <li>
             <RouterLink to="/Abilities" class="block py-2 px-3 rounded hover:text-hover md:bg-transparent" aria-current="page">
               Abilities
@@ -41,12 +46,8 @@ function themeChanger(){
           </li>
           <li>
             <button type="button" @click="themeChanger()">
-              <svg v-if="props.theme==='light'" class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                <path fill-rule="evenodd" d="M13 3a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0V3ZM6.343 4.929A1 1 0 0 0 4.93 6.343l1.414 1.414a1 1 0 0 0 1.414-1.414L6.343 4.929Zm12.728 1.414a1 1 0 0 0-1.414-1.414l-1.414 1.414a1 1 0 0 0 1.414 1.414l1.414-1.414ZM12 7a5 5 0 1 0 0 10 5 5 0 0 0 0-10Zm-9 4a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2H3Zm16 0a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2h-2ZM7.757 17.657a1 1 0 1 0-1.414-1.414l-1.414 1.414a1 1 0 1 0 1.414 1.414l1.414-1.414Zm9.9-1.414a1 1 0 0 0-1.414 1.414l1.414 1.414a1 1 0 0 0 1.414-1.414l-1.414-1.414ZM13 19a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0v-2Z" clip-rule="evenodd"/>
-              </svg>
-              <svg v-else class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                <path fill-rule="evenodd" d="M11.675 2.015a.998.998 0 0 0-.403.011C6.09 2.4 2 6.722 2 12c0 5.523 4.477 10 10 10 4.356 0 8.058-2.784 9.43-6.667a1 1 0 0 0-1.02-1.33c-.08.006-.105.005-.127.005h-.001l-.028-.002A5.227 5.227 0 0 0 20 14a8 8 0 0 1-8-8c0-.952.121-1.752.404-2.558a.996.996 0 0 0 .096-.428V3a1 1 0 0 0-.825-.985Z" clip-rule="evenodd"/>
-              </svg>
+              <FontAwesomeIcon class="text-2xl" v-if="props.theme==='light'" :icon="faSun"/>
+              <FontAwesomeIcon class="text-2xl" v-else :icon="faMoon"/>
             </button>
           </li>
         </ul>
