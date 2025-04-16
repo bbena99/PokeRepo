c59be652f36b9643e0d37466da8a6774afc5f608
Brock Bena <bbena99@gmail.com>
2025-04-16 11:26:03 -0500

    Added abilities list link

--- Client/src/pages/index.vue
+++ Client/src/pages/index.vue
@@ -3,11 +3,12 @@ import { RouterLink } from 'vue-router';
 import { faShare } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { DAMAGETYPES } from '../constants';
+import abilitycapsule from '../assets/images/abilitycapsule.png';
 const moveArray = DAMAGETYPES.slice(1)
 </script>
 
 <template>
-<div class="flex justify-center flex-wrap w-full h-full">
+<div class="flex justify-center content-start flex-wrap w-full h-full">
   <!-- Jumbotron -->
   <div class="flex justify-center flex-wrap w-full h-96 my-8 p-8 rounded-3xl bg-bg2">
     <h1 class="flex justify-center w-full text-header text-5xl font-bold">
@@ -17,7 +18,7 @@ const moveArray = DAMAGETYPES.slice(1)
       The goal of this website is to provide pokemon fans with a wiki that looks nice and shows as much prevalent details about the pokemon they are looking for data on.
     </span>
   </div>
-  <div class="grid grid-cols-6 gap-4 w-full md:w-5/6 lg:w-4/5 2xl:w-3/4 h-96">
+  <div class="grid grid-cols-6 gap-4 w-full md:w-5/6 lg:w-4/5 2xl:w-3/4">
     <RouterLink to="/pokemon" class="col-span-6 md:col-span-3 lg:col-span-2 flex flex-wrap justify-center justify-items-center w-full h-full bg-bg1 shadow-xl rounded-xl overflow-hidden md:hover:scale-105 hover:shadow-2xl hover:text-hover transition-all">
       <img
         src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
@@ -40,6 +41,17 @@ const moveArray = DAMAGETYPES.slice(1)
       </div>
       <span class="flex justify-center items-center md:w-full h-full md:h-1/3 text-xl">
         Move list &nbsp;
+        <FontAwesomeIcon :icon="faShare"/>
+      </span>
+    </RouterLink>
+    <RouterLink to="/ability" class="col-span-6 md:col-span-3 lg:col-span-2 flex flex-wrap justify-center justify-items-center w-full h-full bg-bg1 shadow-xl rounded-xl overflow-hidden md:hover:scale-105 hover:shadow-2xl hover:text-hover transition-all">
+      <img
+        :src="abilitycapsule"
+        alt="Pikachu.png"
+        class="md:h-1/2 md:mt-12"
+        >
+      <span class="flex justify-center items-center md:w-full h-full md:h-1/3 text-xl">
+        Ability list &nbsp;
         <FontAwesomeIcon :icon="faShare"/>
       </span>
     </RouterLink>
